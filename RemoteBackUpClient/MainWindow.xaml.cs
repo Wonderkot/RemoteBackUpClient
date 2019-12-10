6c0fdfba9d0a748db1d920885610ff89b1230795
Wonderkot <dante4645@yandex.ru>
2019-12-10 15:14:21 +0400

    Add project files.

---
+++ RemoteBackUpClient/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+
+
+namespace RemoteBackUpClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnStateChanged(object sender, EventArgs e)
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+                    ShowInTaskbar = true;
+                    break;
+                case WindowState.Minimized:
+                    ShowInTaskbar = false;
+                    break;
+                case WindowState.Maximized:
+                    ShowInTaskbar = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void TaskBarIcon_OnTrayLeftMouseUp(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+        }
+    }
+}
