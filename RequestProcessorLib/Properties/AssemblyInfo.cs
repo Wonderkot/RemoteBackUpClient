01cb660042ffafd7a2d1a4164cbc4d0828baf69e
Wonderkot <dante4645@yandex.ru>
2019-12-17 10:47:34 +0400

    installer

--- RequestProcessorLib/Properties/AssemblyInfo.cs
+++ RequestProcessorLib/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RequestProcessorLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("YobaSoft")]
 [assembly: AssemblyProduct("RequestProcessorLib")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
